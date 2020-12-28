56259e3bfab735d9ad223c3e7da03d7e73925023
JanZinch <jan.zinchanka@mail.ru>
2020-12-28 21:49:15 +0300

    Begin

---
+++ Drone-2D/Assets/Scripts/Controller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] private float screwForce = 25.0f;
+    [SerializeField] private float ForwardMovementSpeed = 10.0f;
+
+    private Transform drone;
+    private Rigidbody2D body;
+
+    const float lowerBorder = -3.0f;
+
+    void Start()
+    {
+        drone = GetComponent<Transform>();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        bool screwActive = Input.GetMouseButton(0);
+
+        Vector2 newVelocity = body.velocity;
+        newVelocity.x = ForwardMovementSpeed;
+        body.velocity = newVelocity;
+
+        if (screwActive || drone.position.y <= lowerBorder) {
+
+            body.AddForce(new Vector2(0, screwForce));
+        }
+
+
+
+    }
+}
